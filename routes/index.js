9621dbbe175ece8bb21bf7fdb6dcc3d2d2591a3d
veraexequiel <veraexequiel5@gmail.com>
2023-03-20 17:33:47 -0700

    added get and post requests to server code, for now in-memory

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+let reviews = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/reviews', function(req, res, next) {
+  res.send(reviews);
+});
+
+router.post('/reviews', function(req, res, next) {
+  reviews.push(req.body);
+  res.status(201).send(req.body);
 });
 
 module.exports = router;
