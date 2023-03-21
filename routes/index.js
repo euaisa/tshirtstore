81630de3c2d90b9e60847cbdb7d93f5ccebe23a0
veraexequiel <veraexequiel5@gmail.com>
2023-03-20 22:49:09 -0700

    now reviews are loaded from and saved to a json file on the server.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
 let reviews = [];
+
+function loadReviews() {
+  let data = fs.readFileSync('reviews.json', 'utf8');
+  reviews = JSON.parse(data);
+}
+
+function saveReviews() {
+  fs.writeFileSync('reviews.json', JSON.stringify(reviews, null, 4), 'utf8');
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,11 +19,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/reviews', function(req, res, next) {
+  loadReviews();
   res.send(reviews);
 });
 
 router.post('/reviews', function(req, res, next) {
+  loadReviews();
   reviews.push(req.body);
+  saveReviews();
   res.status(201).json(req.body);
 });
 
